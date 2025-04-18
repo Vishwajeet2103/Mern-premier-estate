dc11a348da8f6f9913f7a8daf69de7c241c7e695
Vishwajeet Singh <vd0619@srmist.edu.in>
2025-04-18 14:07:14 +0530

    Update verifyUser.js

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -2,9 +2,13 @@ import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
+  const authHeader = req.headers.authorization;
 
-  if (!token) return next(errorHandler(401, 'Unauthorized'));
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return next(errorHandler(401, 'Unauthorized'));
+  }
+
+  const token = authHeader.split(' ')[1];
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, 'Forbidden'));
@@ -13,3 +17,4 @@ export const verifyToken = (req, res, next) => {
     next();
   });
 };
+
